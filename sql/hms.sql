effe1d054a79418014a5fcb5cbca3f5f0e892296
Keyan Andy Delgado <96453690+2ez4Keyan@users.noreply.github.com>
2024-05-24 12:13:08 +0800

    qkweokqwe

--- sql/hms.sql
+++ sql/hms.sql
@@ -1,87 +1,81 @@
--- MySQL dump 10.13  Distrib 8.0.26, for macos11.4 (arm64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: mohityadav.codes    Database: hms
--- ------------------------------------------------------
--- Server version	5.5.5-10.5.12-MariaDB-0ubuntu0.21.04.1
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2024 at 06:11 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `guests`
 --
 
-CREATE DATABASE IF NOT EXISTS hms;
-use hms;
-DROP TABLE IF EXISTS `guests`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `guests` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(30) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
   `email_id` varchar(50) DEFAULT NULL,
   `phone` bigint(20) DEFAULT NULL,
   `city` varchar(20) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `guests`
 --
 
-LOCK TABLES `guests` WRITE;
-/*!40000 ALTER TABLE `guests` DISABLE KEYS */;
-INSERT INTO `guests` VALUES (3,'John Doe','US','john@doe.com',1231231231,"NYC",'2021-10-14 08:51:19'),(4,'Mohit Yadav','India','mohit@mohit.yayy.me',1111111111,"Jaipur",'2021-10-17 05:19:02'),(5,'Anirudh','NMS','anisin300@gmail.com',9191919191,"Jaipur",'2021-10-17 06:58:23');
-/*!40000 ALTER TABLE `guests` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `guests` (`id`, `name`, `address`, `email_id`, `phone`, `city`, `created_at`) VALUES
+(6, 'Jener', 'tupi', 'jener@gmail.com', 11, NULL, '2024-05-22 20:38:15'),
+(7, 'Keyan Andy Delgado', 'Sto nino', 'keyaandydelgado@gmail.com', 9262408442, NULL, '2024-05-23 12:46:33'),
+(8, 'jimbo', 'tuka', 'jimbo@gmail.com', 123, NULL, '2024-05-23 12:50:46'),
+(9, 'test', 'test', 'test@gmail.com', 123, NULL, '2024-05-23 12:54:55');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `login`
 --
 
-DROP TABLE IF EXISTS `login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
   `username` varchar(15) NOT NULL,
   `password` varchar(10) NOT NULL,
-  `sec_que` varchar(100) NULL,
-  `sec_ans` varchar(30) NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `sec_que` varchar(100) DEFAULT NULL,
+  `sec_ans` varchar(30) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `login`
 --
 
-LOCK TABLES `login` WRITE;
-/*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES ('username','password', NULL, NULL,'2021-08-13 01:34:25');
-/*!40000 ALTER TABLE `login` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `login` (`username`, `password`, `sec_que`, `sec_ans`, `created_at`) VALUES
+('username', 'password', NULL, NULL, '2021-08-13 01:34:25');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `reservations`
 --
 
-DROP TABLE IF EXISTS `reservations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reservations` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `g_id` int(11) DEFAULT NULL,
   `r_date` datetime DEFAULT NULL,
   `check_in` datetime DEFAULT NULL,
@@ -89,61 +83,110 @@ CREATE TABLE `reservations` (
   `meal` tinyint(1) DEFAULT NULL,
   `r_id` int(11) DEFAULT NULL,
   `r_type` char(2) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `FK_guests` (`g_id`),
-  KEY `FK_rooms` (`r_id`),
-  CONSTRAINT `FK_guests` FOREIGN KEY (`g_id`) REFERENCES `guests` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_rooms` FOREIGN KEY (`r_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reservations`
 --
 
-LOCK TABLES `reservations` WRITE;
-/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
-INSERT INTO `reservations` VALUES (12,4,'2021-10-15 00:00:00','2021-10-15 00:00:00','2021-10-15 00:00:00',0,3,'B','2021-10-15 07:05:05'),(13,3,NULL,'2021-10-17 05:33:05',NULL,1,1,NULL,'2021-10-17 05:33:05');
-/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `reservations` (`id`, `g_id`, `r_date`, `check_in`, `check_out`, `meal`, `r_id`, `r_type`, `created_at`) VALUES
+(18, 8, NULL, '2024-05-23 12:51:02', '2024-05-23 12:51:19', 1, 12, NULL, '2024-05-23 12:51:02'),
+(24, 7, NULL, '2024-05-23 13:17:03', NULL, 1, 1, NULL, '2024-05-23 13:17:03');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `rooms`
 --
 
-DROP TABLE IF EXISTS `rooms`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `room_no` int(11) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `room_type` char(2) DEFAULT NULL,
   `currently_booked` tinyint(1) DEFAULT 0,
-  `created_at` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `room_no` (`room_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `rooms`
 --
 
-LOCK TABLES `rooms` WRITE;
-/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES (1,1,4500,'D',0,'2021-10-15 07:05:03'),(3,2,3402,'D',0,'2021-10-16 10:38:49'),(4,10,2300,'D',0,'2021-10-17 05:15:29'),(5,4,3124,'N',0,'2021-10-17 05:15:38'),(6,69,3241,'N',0,'2021-10-17 05:16:09'),(7,3,4000,'D',0,'2021-10-17 05:16:33'),(9,5,2341,'D',0,'2021-10-17 05:17:29'),(10,21,3045,'D',0,'2021-10-17 06:57:46');
-/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `rooms` (`id`, `room_no`, `price`, `room_type`, `currently_booked`, `created_at`) VALUES
+(1, 1, 4500, 'D', 0, '2021-10-15 07:05:03'),
+(4, 10, 2300, 'D', 0, '2021-10-17 05:15:29'),
+(5, 4, 3124, 'N', 0, '2021-10-17 05:15:38'),
+(6, 69, 3241, 'N', 0, '2021-10-17 05:16:09'),
+(7, 3, 4000, 'D', 0, '2021-10-17 05:16:33'),
+(9, 5, 2341, 'D', 0, '2021-10-17 05:17:29'),
+(12, 22, 1200, 'D', 0, '2024-05-23 12:45:26');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `guests`
+--
+ALTER TABLE `guests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_guests` (`g_id`),
+  ADD KEY `FK_rooms` (`r_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `room_no` (`room_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `guests`
+--
+ALTER TABLE `guests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `FK_guests` FOREIGN KEY (`g_id`) REFERENCES `guests` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_rooms` FOREIGN KEY (`r_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-10-18  9:28:53
